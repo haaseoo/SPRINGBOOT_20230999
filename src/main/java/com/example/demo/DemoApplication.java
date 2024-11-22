43e19d5c1d3a1aad28a615eba21722668394150f
haaseoo <sojun1098@naver.com>
2024-11-22 17:36:00 +0900

    2주차

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) { // 메인 메서드, 프로그램의 시작
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
