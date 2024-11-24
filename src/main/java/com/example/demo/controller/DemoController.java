7fa028b010bcdb3ae718b6df3ad5d1d1ca28501a
haaseoo <sojun1098@naver.com>
2024-11-24 19:16:51 +0900

    4주차 완료

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,14 +1,21 @@
-package com.example.demo;
+package com.example.demo.controller;
 
-import java.time.LocalDate;
 
+import com.example.demo.model.service.TestService;
+import com.example.demo.model.domain.TestDB;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import java.time.LocalDate;
+import java.util.List;
 
 @Controller // 컨트롤러 어노테이션 명시
-
 public class DemoController {
+
+  @Autowired
+  TestService testService;
 
   @GetMapping("/") // 기본 루트 경로 처리
   public String index() {
@@ -34,5 +41,13 @@ public class DemoController {
     @GetMapping("/about_detailed") // 전송 방식 GET
     public String about_detailed(Model model) {
       return "about_detailed"; // hello.html 연결
+    }
+
+    @GetMapping("/testdb")
+    public String getAllTestDBs(Model model) {
+      List<TestDB> users = testService.findAll();
+      model.addAttribute("users", users);
+      System.out.println("데이터 출력 디버그 : " + users);
+        return "testdb";
     }
 }
